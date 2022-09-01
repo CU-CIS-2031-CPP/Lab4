7d31a1e8e5e5230cea84e9ecc3e4035378548cac
Bill Kuker <bkuker@billkuker.com>
2022-09-01 08:08:01 -0400

    Lab3

---
+++ Functions.cpp
@@ -0,0 +1,34 @@
+//Recognize these old friends?
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl; 
+using std::string;
+
+//Declare AND define!
+int add(int a, int b){
+   int result = a;
+   result = result + b;
+   return result;
+}
+
+//Declare
+int multiply(int, int);
+
+//Declare
+float divide(int divident, int divisor);
+
+void main(){
+   cout << add(4,5) << endl;
+   cout << add(1,3) << endl;
+   cout << divide(1,2) << endl;
+}
+
+//Define
+int multiply(int a, int b){
+   return a * b;
+}
+
+float divide(int dividend, int divisor){
+   return dividend / divisor;
+}
